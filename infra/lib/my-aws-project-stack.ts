aa3451753de78dda2351d4cdf84708fad6a95918
tvarun98 <tvarun98@gmail.com>
2024-12-18 15:54:46 -0500

    Commit 1
    API GW + Lambda + DynamoDB

---
+++ infra/lib/my-aws-project-stack.ts
@@ -0,0 +1,39 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export class MyAwsProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // DynamoDB Table
+    const dynamoTable = new dynamodb.Table(this, 'DynamoTable', {
+      partitionKey: { name: 'ID', type: dynamodb.AttributeType.STRING },
+      tableName: 'UserPasswords',
+    });
+
+    // Lambda Function
+    const lambdaFunction = new lambda.Function(this, 'MyLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('service/lambda'),
+      environment: {
+        DYNAMODB_TABLE: dynamoTable.tableName,
+      },
+    });
+
+    // Grant permissions to Lambda
+    dynamoTable.grantReadData(lambdaFunction);
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'MyApi', {
+      restApiName: 'LambdaDynamoApi',
+      description: 'API to fetch passwords from DynamoDB',
+    });
+
+    const getPassword = api.root.addResource('{userID}');
+    getPassword.addMethod('GET', new apigateway.LambdaIntegration(lambdaFunction));
+  }
+}
